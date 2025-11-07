037f77f0a732cec370b4e3e5c12433248c5b292f
Allan Avosa <allanavosa@gmail.com>
2025-11-07 08:14:30 +0300

    Updated test file for database

--- data/test.py
+++ data/test.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+load_dotenv()
 def create_dataframe():
     data= {
         'names':['Alice','Bob','Charlie','David','Eve'],
@@ -16,6 +19,13 @@ def load_to_database(df,user,password,host,port,database,table_name):
     engine = create_engine(db_url)
     df.to_sql(table_name, con=engine, if_exists="append", index=False)
 def main():
+    user = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD")
+    host = os.getenv("DB_HOST")
+    port = os.getenv("DB_PORT")
+    database = os.getenv("DB_NAME")
+    table_name =os.getenv("Table")
+
     
     df = create_dataframe()
     print("Dataframe Created Successfully")
